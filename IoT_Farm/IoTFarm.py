c206b9021c1a8bd839ba489531748b306b64ad23
吳咨賢 <smartrobin881010@gmail.com>
2021-04-23 03:37:11 +0800

    first commit

--- IoT_Farm/IoTFarm.py
+++ IoT_Farm/IoTFarm.py
@@ -1,51 +1,58 @@
-# 1.啟用樹莓派SPI
-# sudo raspi-config
-# Interface選項啟用SPI
-# 重啟樹莓派(輸入reboot)
-# --------------------------------------------------------------------------------
-# 2.安裝spidev庫:光啟用SPI介面,樹莓派還是無法讀取YL-69的值,spidev將幫助讀取
-# sudo apt-get install git python-dev
-# git clone git://github.com/doceme/py-spidev
-# cd py-spidev/
-# sudo python setup.py install
-# --------------------------------------------------------------------------------
-# 3.安裝numpy庫:YL-69獲得的值還是電壓值而非土壤溼度百分比
-# MCP3008 IC接收到的輸出值是0-0123範圍，需將這些值對映到0-100，以得到百分比
-# sudo apt-get install python-numpy
-
 import time
 import math
 import requests as rq
-import Adafruit_DHT
+import adafruit_dht
+from board import *
+import spidev
+from numpy import interp
 
-# import spidev
+DHT_PIN = D4  # DHT連GPIO4
+DHT_SENSOR = adafruit_dht.DHT22(DHT_PIN, use_pulseio=False)  # 定義DHT_SENSOR為DHT22
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4  # DHT pin to GPIO4
+# 連結至SPI
+spi = spidev.SpiDev()
+spi.open(0, 0)
 
-# 連結SPI
-# spi = spidev.SpiDev()
-# spi.open(0, 0)
 
 # 讀取MCP3008的資料
-# def analogInput(channel):
-#   spi.max_speed_hz = 1350000
-#   adc = spi.xfer2([1,(8+channel)<<4,0])
-#   data = ((adc[1]&3) << 8) + adc[2]
-#   return data
+def analogInput(channel):
+    spi.max_speed_hz = 1350000
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    MCP3008_data = ((adc[1] & 3) << 8) + adc[2]
+    return MCP3008_data
+
 
 while True:
-    humi, temp = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)  # 感測溫溼度
-    humi = math.floor(humi * 100) / 100  # 格式化資料至小數點後兩位
-    temp = math.floor(temp * 100) / 100
-    # mois = analogInput(0)  # Reading from CH0
-    # mois = interp(output, [0, 1023], [100, 0])
-    # mois = int(output)
-    url = 'http://140.117.71.98:8000/api/humidity/'  # API位置
-    data = {'value': humi, 'sensorId': temp}  # mois加入後delete
-    # data = {'iotHumidity': humi, 'iotTemperature': temp, 'iotSoilMoisture':mois}  # mois加入後加入
-    print('溫度:%.2f°C' % temp)  # print格式化後的temp
-    print('濕度:%.2f%%' % humi)  # print格式化後的humi
-    # print('土壤濕度:%.2f%%' % mois)  # print格式化後的mois
-    res = rq.post(url=url, data=data)  # Post至API
-    time.sleep(5)  # delay 5 sec
+    try:
+        # DHT-22資料讀取
+        dht22_humi = DHT_SENSOR.humidity
+        dht22_humi = math.floor(dht22_humi * 100) / 100  # 格式化資料至小數點後兩位
+        dht22_temp = DHT_SENSOR.temperature
+        dht22_temp = math.floor(dht22_temp * 100) / 100
+        dht22_index = 1  # 暫時沒辦法處理 先假設1
+
+        # YL-69資料讀取
+        mois = analogInput(0)  # Reading from CH0
+        mois = interp(mois, [0, 1023], [100, 0])
+        mois = int(mois)
+
+        # API資料
+        dht22_url = 'http://140.117.71.98:8000/api/humidtemp/'  # API位置
+        yl69_url = 'http://140.117.71.98:8000/api/moisture/'
+        dht22_data = {'humidity': dht22_humi, 'temperature': dht22_temp, 'heatIndex': dht22_index}  # dht22 data
+        yl69_data = {'value': mois}  # yl69 data
+
+        # 輸出
+        print('溫度:%.2f°C' % dht22_temp)  # print格式化後的temp
+        print('濕度:%.2f%%' % dht22_humi)  # print格式化後的humi
+        print('土壤濕度:%.2f%%' % mois)  # print格式化後的mois
+        res = rq.post(url=dht22_url, data=dht22_data)  # Post至API
+        print(res)
+        res = rq.post(url=yl69_url, data=yl69_data)  # Post至API
+        print(res)
+        print('\n')
+    except RuntimeError as error:  # 資料讀取失敗時
+        print(error.args[0])
+        print('\n')
+
+    time.sleep(5.0)  # delay 5 sec
