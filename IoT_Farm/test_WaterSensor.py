5d81308a581a6b7aa13a2912e768345fb3be565e
吳咨賢 <smartrobin881010@gmail.com>
2021-05-18 22:51:52 +0800

    Water Sensor code test

--- IoT_Farm/test_WaterSensor.py
+++ IoT_Farm/test_WaterSensor.py
@@ -8,29 +8,26 @@ SPICS = 16
 photo_ch = 0
 
 
-# port init
 def init():
     GPIO.setwarnings(False)
-    GPIO.cleanup()  # clean up at the end of your script
-    GPIO.setmode(GPIO.BCM)  # to specify whilch pin numbering system
-    # set up the SPI interface pins
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(SPIMOSI, GPIO.OUT)
     GPIO.setup(SPIMISO, GPIO.IN)
     GPIO.setup(SPICLK, GPIO.OUT)
     GPIO.setup(SPICS, GPIO.OUT)
 
 
-# read SPI data from MCP3008(or MCP3204) chip,8 possible adc's (0 thru 7)
+# 從MCP3008讀取WaterSensor的資料
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
     if (adcnum > 7) or (adcnum < 0):
         return -1
     GPIO.output(cspin, True)
-    GPIO.output(clockpin, False)  # start clock low
-    GPIO.output(cspin, False)  # bring CS low
-
+    GPIO.output(clockpin, False)
+    GPIO.output(cspin, False)
     commandout = adcnum
-    commandout |= 0x18  # start bit + single-ended bit
-    commandout <<= 3  # we only need to send 5 bits here
+    commandout |= 0x18
+    commandout <<= 3
     for i in range(5):
         if commandout & 0x80:
             GPIO.output(mosipin, True)
@@ -39,19 +36,15 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
         commandout <<= 1
         GPIO.output(clockpin, True)
         GPIO.output(clockpin, False)
-
     adcout = 0
-    # read in one empty bit, one null bit and 10 ADC bits
     for i in range(12):
         GPIO.output(clockpin, True)
         GPIO.output(clockpin, False)
         adcout <<= 1
         if GPIO.input(misopin):
             adcout |= 0x1
-
     GPIO.output(cspin, True)
-
-    adcout >>= 1  # first bit is 'null' so drop it
+    adcout >>= 1
     return adcout
 
 
@@ -68,7 +61,6 @@ def main():
         elif 30 <= adc_value < 200:
             print("it is water flow")
             print("water level:" + str("%.1f" % (adc_value / 200. * 100)) + "%\n")
-        # print "adc_value= " +str(adc_value)+"\n"
         time.sleep(1)
 
 
