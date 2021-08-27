00fd47e2ea8621c3b5a756c34199aebd4974bfc4
吳咨賢 <robin1010552@gmail.com>
2021-08-27 23:22:20 +0800

    Slider program update

--- IoT_Farm/RaspberryPi/test_StepperMotor.py
+++ IoT_Farm/RaspberryPi/test_StepperMotor.py
@@ -1,46 +1,62 @@
-from flask import Flask, render_template_string, request 
+from flask import Flask, render_template_string, request
 from time import sleep
 import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
 
-GPIO_pins = (14, 15, 18) # Microstep Resolution MS1-MS3 -> GPIO Pin
-direction = 20       # Direction -> GPIO Pin
-step = 21      # Step -> GPIO Pin
+GPIO_pins = (14, 15, 18)  # Microstep Resolution MS1-MS3 -> GPIO Pin
+direction = 20  # Direction -> GPIO Pin
+step = 21  # Step -> GPIO Pin
+position = 0  # 現在為手動調整
+# position = rq.get
 
 mymotortest = RpiMotorLib.A4988Nema(direction, step, GPIO_pins, "A4988")
 
 app = Flask(__name__)
-
 
 TPL = '''
 <html>
     <head><title>滑軌控制</title></head>
     <body>
         <form method="POST" action="test">
-            <p><input type="range" min="1" max="20" name="slider"/></p>
+            <p><input type="range" min="1" max="2" name="slider"/></p>
             <input type="submit" value="submit"/>
         </form>
     </body>
 </html>
 '''
- 
+
+
 @app.route("/")
 def home():
-
     return render_template_string(TPL)
- 
+
+
 @app.route("/test", methods=["POST"])
 def test():
     slider = request.form["slider"]
-    print(int(slider))
-  
-    if (int(slider)>10):
-       mymotortest.motor_go(True, "Full" , 600,int(slider)*.0004, False, .05)
-    
-    if (int(slider)<10):
-       mymotortest.motor_go(False, "Full" , 600,int(slider)*.001, False, .05)
+    global position
+    if int(slider) == 2:
+        if position == 10:
+            print('Cannot move on')
+            print(position)
+        else:
+            mymotortest.motor_go(True, "Full", 100, 0.01, False, .05)
+            position = position + 1
+            print(position)
+            # rq.post(url = '', data = 'position')
+
+    if int(slider) == 1:
+        if position == 0:
+            print('Cannot move on')
+            print(position)
+        else:
+            mymotortest.motor_go(False, "Full", 100, 0.01, False, .05)
+            position = position - 1
+            print(position)
+            # rq.post(url = '', data = 'position')
 
     return render_template_string(TPL)
+
 
 if __name__ == "__main__":
     app.run()
