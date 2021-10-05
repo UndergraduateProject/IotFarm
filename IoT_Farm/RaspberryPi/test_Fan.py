dd73a78accbaa08b3a10a5f948b192b79388b2c0
Nyc <s2012439@yes.my>
2021-10-05 13:23:55 +0800

    fan.py

--- IoT_Farm/RaspberryPi/test_Fan.py
+++ IoT_Farm/RaspberryPi/test_Fan.py
@@ -1,4 +1,6 @@
-import RPi.GPIO as GPIO
+import socketio
+import requests as rq
+# import RPi.GPIO as GPIO
 import time
 
 fan_pin = 18
@@ -8,8 +10,26 @@ GPIO.setup(fan_pin, GPIO.OUT)
 flag = 0
 GPIO.output(fan_pin, flag)
 
+#socket
+sio = socketio.Client()
+
+@sio.on('connect')
+def on_connect():
+    print('connection established')
+
+@sio.on("slider")
+def on_message(data):
+    print('message received with ', data)
+    sio.emit('fan', "message received")
+
+@sio.on('disconnect')
+def on_disconnect():
+    print('disconnected from server')
+
 while True:
-    # temp = rq.get  # 從DHT22偵測的溫度中獲取
+    temp = rq.get("http://140.117.71.98:8000/api/ActionCondition/3/")
+    print(temp)
+    # 從DHT22偵測的溫度中獲取
     temp = 30  # 假設
     if temp > 25:
         flag = 1
